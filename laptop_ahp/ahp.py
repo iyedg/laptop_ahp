07c2832b924be4bf5dc14c6ca0f6e3f9c428d8c9
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2018-08-22 23:32:45 +0100

    use integer values

--- laptop_ahp/ahp.py
+++ laptop_ahp/ahp.py
@@ -1,7 +1,8 @@
+import math
 from itertools import combinations_with_replacement
-import pandas as pd
 
 import ipywidgets as widgets
+import pandas as pd
 from IPython.display import display
 
 
@@ -31,18 +32,18 @@ class AHP:
         """
         if change["type"] == "change":
             owner = change["owner"]
-            val = float(change["new"])
+            val = int(change["new"])
             i, j = self.slider_pair[owner]
             compare_sentence = self._compare_sentence(i, j, val)
             if val <= 0:
                 # Account for -8 being the smallet value by adding 1 to its
                 # absolute value
-                val = round(1 / (abs(val) + 1), 1)
+                val = 1 / (abs(val) + 1)
                 i, j = j, i
                 compare_sentence = self._compare_sentence(
-                    i, j, round(1 / val, 1))
+                    i, j, int(1 / val))
             self.pairwise_df.loc[i, j] = val
-            self.pairwise_df.loc[j, i] = 1 / val
+            self.pairwise_df.loc[j, i] = int(math.ceil(1 / val))
             self.slider_caption[owner].value = compare_sentence
 
     def display(self):
@@ -61,7 +62,7 @@ class AHP:
             # The scale ranges from 1 to 9 in both positive and negative
             # directions. A minimum of 8 is used, the value will be adjusted
             # for negative values when calculating
-            slider = widgets.FloatSlider(value=1, min=-8, max=9, readout=False)
+            slider = widgets.IntSlider(value=1, min=-8, max=9, readout=False)
             caption = widgets.HTML(
                 value=self._compare_sentence(i, j, slider.value))
             self.slider_caption[slider] = caption
